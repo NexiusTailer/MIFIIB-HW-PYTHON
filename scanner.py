e530f663f983286c494b4521f76815df37fc824b
Nexius <nexius.beslam@mail.ru>
2023-03-30 02:36:13 +0300

    Добавлен http.server
    - тестовый вариант с хандлером, который должен слушать порт
    - пофиксил возвращаемые значения в функции send_http_request

--- scanner.py
+++ scanner.py
@@ -1,5 +1,6 @@
 import os
 import platform
+from http.server import HTTPServer, BaseHTTPRequestHandler
 import json
 import requests
 
@@ -30,7 +31,7 @@ def do_ping_sweep(ip_addr, num_of_hosts = 1):
 
     return scan_dict
 
-def sent_http_request(url, method = "GET", headers = None, payload = None):
+def send_http_request(url, method = "GET", headers = None, payload = None):
     headers_dict = {}
 
     if headers:
@@ -52,6 +53,35 @@ def sent_http_request(url, method = "GET", headers = None, payload = None):
             f"[#] Response content:\n {result.text}"
         )
 
-        return {"Status" : result.status_code, "Headers" : result.headers}
+        return {
+            "Status" : result.status_code,
+            "Headers" : result.headers,
+            "Content" : result.text
+        }
 
     return {}
+
+class ProcessHandler(BaseHTTPRequestHandler):
+    def set_headers(self):
+        self.send_response(200)
+        self.send_header("Content-Type", "text/json")
+        length = int(self.headers["Content-Length"])
+        content = self.rfile.read(length)
+        temp = str(content).strip('b\'')
+        self.end_headers()
+        return temp
+
+    def do_GET(self):
+        temp = self.set_headers()
+        payload = json.loads(temp)
+        result = do_ping_sweep(payload["ip_address"], payload["num_of_hosts"])
+        self.wfile.write(f"Result of scanning: {result}".encode())
+
+    def do_POST(self):
+        temp = self.set_headers()
+        payload = json.loads(temp)
+        response = send_http_request(payload["target"], payload["method"])
+        self.wfile.write(f"HTTP response: {response}".encode())
+
+httpd = HTTPServer(("0.0.0.0", 8081), ProcessHandler)
+httpd.serve_forever()
