b33134b8f0df310c2132522372fab3bd88fe774d
Nexius <nexius.beslam@mail.ru>
2023-03-30 05:10:13 +0300

    Поправлены вылеты после первых тестов

--- scanner.py
+++ scanner.py
@@ -65,23 +65,40 @@ class ProcessHandler(BaseHTTPRequestHandler):
     def set_headers(self):
         self.send_response(200)
         self.send_header("Content-Type", "text/json")
-        length = int(self.headers["Content-Length"])
-        content = self.rfile.read(length)
-        temp = str(content).strip('b\'')
+
+        length = int(self.headers.get("Content-Length", 0)) or None
+
+        if not length:
+            temp = None
+        else:
+            content = self.rfile.read(length)
+            temp = str(content).strip('b\'')
+
         self.end_headers()
+
         return temp
 
     def do_GET(self):
         temp = self.set_headers()
-        payload = json.loads(temp)
-        result = do_ping_sweep(payload["ip_address"], payload["num_of_hosts"])
-        self.wfile.write(f"Result of scanning: {result}".encode())
+
+        if not temp:
+            self.wfile.write("Failed, probably bad request".encode())
+        else:
+            payload = json.loads(temp)
+
+            result = do_ping_sweep(payload["ip_address"], payload["num_of_hosts"])
+            self.wfile.write(f"Result of scanning: {result}".encode())
 
     def do_POST(self):
         temp = self.set_headers()
-        payload = json.loads(temp)
-        response = send_http_request(payload["target"], payload["method"])
-        self.wfile.write(f"HTTP response: {response}".encode())
+
+        if not temp:
+            self.wfile.write("Failed, probably bad request".encode())
+        else:
+            payload = json.loads(temp)
+
+            response = send_http_request(payload["target"], payload["method"])
+            self.wfile.write(f"HTTP response: {response}".encode())
 
 httpd = HTTPServer(("0.0.0.0", 8081), ProcessHandler)
 httpd.serve_forever()
